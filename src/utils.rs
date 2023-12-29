c9c19570c63ae4cf130af049e96bbcf05e1402ad
Cameron Dehning <bedsteler2.0@gmail.com>
2023-12-29 14:38:52 -0600

    Refactor get_client function to handle client configuration

--- src/utils.rs
+++ src/utils.rs
@@ -4,7 +4,27 @@ use serde::de;
 pub(crate) static CLIENT: OnceCell<&reqwest::Client> = OnceCell::new();
 
 pub fn get_client() -> &'static reqwest::Client {
-    CLIENT.get().expect("flathub-rs: client not configured")
+    if let Some(client) = CLIENT.get() {
+        client
+    } else  {
+        let client = reqwest::Client::builder()
+            .default_headers({
+                let mut headers = reqwest::header::HeaderMap::new();
+                headers.insert(
+                    reqwest::header::USER_AGENT,
+                    reqwest::header::HeaderValue::from_static("flathub-rs"),
+                );
+                headers.insert(
+                    reqwest::header::ACCEPT, 
+                    reqwest::header::HeaderValue::from_static("application/json")
+                );
+                headers
+            })
+            .build()
+            .unwrap();
+        configure_client(Box::leak(Box::new(client)));
+        CLIENT.get().unwrap()
+    }
 }
 
 pub fn configure_client(client: &'static reqwest::Client) {
